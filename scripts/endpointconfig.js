b52cebd9ab881744a3cf32fd899259c2c6d14e6f
guptapawan <guptapawan@adobe.com>
2025-08-08 11:08:40 +1000

    update files

---
+++ scripts/endpointconfig.js
@@ -0,0 +1,9 @@
+function getAEMPublish() {
+  return 'https://publish-p137117-e1400714.adobeaemcloud.com';
+}
+
+function getAEMAuthor() {
+  return 'https://author-p137117-e1400714.adobeaemcloud.com';
+}
+
+export { getAEMPublish, getAEMAuthor };
