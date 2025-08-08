91d015cf2c226de1219fd5385f07dbb596db6d57
guptapawan <guptapawan@adobe.com>
2025-08-08 12:44:00 +1000

    update files

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,11 @@ module.exports = {
   rules: {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'no-param-reassign': [2, { props: false }],
+    'xwalk/max-cells': ['error', {
+      '*': 8,
+      'a-specific-model': 8,
+      'another-specific-model': 2,
+    }], // allow modifying properties of param
   },
 };
